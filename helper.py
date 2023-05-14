10263cbc8c6d626e7851a19d0c0f42ea3dc96eb0
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-05-14 22:58:08 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,59 @@
+def unidecode_text(text):
+    try:
+        #pdb.set_trace()
+        text = unidecode.unidecode(text)
+    except:
+        pass
+    return text
+
+def correct_grape_names(row):
+    regexp = [r'shiraz', r'ugni blanc', r'cinsaut', r'carinyena', r'^ribolla$', r'palomino', r'turbiana', r'verdelho', r'viura', r'pinot bianco|weissburgunder', r'garganega|grecanico', r'moscatel', r'moscato', r'melon de bourgogne', r'trajadura|trincadeira', r'cannonau|garnacha', r'grauburgunder|pinot grigio', r'pinot noir|pinot nero', r'colorino', r'mataro|monastrell', r'mourv(\w+)']
+    grapename = ['syrah', 'trebbiano', 'cinsault', 'carignan', 'ribolla gialla', 'palomino','verdicchio', 'verdejo','macabeo', 'pinot blanc', 'garganega', 'muscatel', 'muscat', 'muscadet', 'treixadura', 'grenache', 'pinot gris', 'pinot noir', 'lambrusco', 'mourvedre', 'mourvedre']
+    f = row
+    for exsearch, gname in zip(regexp, grapename):
+        f = re.sub(exsearch, gname, f)
+    return f
+  
+  def is_noun(tag):
+    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
+
+
+def is_verb(tag):
+    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+
+def is_adverb(tag):
+    return tag in ['RB', 'RBR', 'RBS']
+
+
+def is_adjective(tag):
+    return tag in ['JJ', 'JJR', 'JJS']
+
+# transform tag forms
+def penn_to_wn(tag):
+    if is_adjective(tag):
+        return wordnet.ADJ
+    elif is_noun(tag):
+        return wordnet.NOUN
+    elif is_adverb(tag):
+        return wordnet.ADV
+    elif is_verb(tag):
+        return wordnet.VERB
+    return wordnet.NOUN
+  
+  def print_stats(preds, target, labels, sep='-', sep_len=40, fig_size=(10,8)):
+    print('Accuracy = %.3f' % metrics.accuracy_score(target, preds))
+    print(sep*sep_len)
+    print('Classification report:')
+    print(metrics.classification_report(target, preds))
+    print(sep*sep_len)
+    print('Confusion matrix')
+    cm=metrics.confusion_matrix(target, preds)
+    cm = cm / np.sum(cm, axis=1)[:,None]
+    sns.set(rc={'figure.figsize':fig_size})
+    sns.heatmap(cm, 
+        xticklabels=labels,
+        yticklabels=labels,
+           annot=True, cmap = 'YlGnBu')
+    plt.pause(0.05)
+    
