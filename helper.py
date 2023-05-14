317de0088923624577d14b39d47db9fae1468603
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-05-15 00:02:57 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,3 +1,14 @@
+from nltk.corpus import wordnet
+from sklearn import metrics
+import numpy as np
+import seaborn as sns
+import unidecode
+import re
+
+# list of word types (nouns and adjectives) to leave in the text
+defTags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJS', 'JJR']#, 'RB', 'RBS', 'RBR', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+#Conver text to unicode
 def unidecode_text(text):
     try:
         #pdb.set_trace()
@@ -6,6 +17,7 @@ def unidecode_text(text):
         pass
     return text
 
+# Correction of wine names in different Countries/region
 def correct_grape_names(row):
     regexp = [r'shiraz', r'ugni blanc', r'cinsaut', r'carinyena', r'^ribolla$', r'palomino', r'turbiana', r'verdelho', r'viura', r'pinot bianco|weissburgunder', r'garganega|grecanico', r'moscatel', r'moscato', r'melon de bourgogne', r'trajadura|trincadeira', r'cannonau|garnacha', r'grauburgunder|pinot grigio', r'pinot noir|pinot nero', r'colorino', r'mataro|monastrell', r'mourv(\w+)']
     grapename = ['syrah', 'trebbiano', 'cinsault', 'carignan', 'ribolla gialla', 'palomino','verdicchio', 'verdejo','macabeo', 'pinot blanc', 'garganega', 'muscatel', 'muscat', 'muscadet', 'treixadura', 'grenache', 'pinot gris', 'pinot noir', 'lambrusco', 'mourvedre', 'mourvedre']
@@ -14,20 +26,19 @@ def correct_grape_names(row):
         f = re.sub(exsearch, gname, f)
     return f
   
-  def is_noun(tag):
+#pos tagging
+def is_noun(tag):
     return tag in ['NN', 'NNS', 'NNP', 'NNPS']
-
 
 def is_verb(tag):
     return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 
-
 def is_adverb(tag):
     return tag in ['RB', 'RBR', 'RBS']
 
-
 def is_adjective(tag):
     return tag in ['JJ', 'JJR', 'JJS']
+
 
 # transform tag forms
 def penn_to_wn(tag):
@@ -41,7 +52,8 @@ def penn_to_wn(tag):
         return wordnet.VERB
     return wordnet.NOUN
   
-  def print_stats(preds, target, labels, sep='-', sep_len=40, fig_size=(10,8)):
+ #print stats of models   
+def print_stats(preds, target, labels, sep='-', sep_len=40, fig_size=(10,8)):
     print('Accuracy = %.3f' % metrics.accuracy_score(target, preds))
     print(sep*sep_len)
     print('Classification report:')
